34c6799d6d83b9df42210da8a3902d1795be4512
Melina Boedecker <melinamboedecker@gmail.com>
2021-08-22 00:05:08 -0700

    put individual project data in display

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,7 +4,17 @@ const Project = ({ projects }) => {
 
     return ( 
         <div>
-
+            <div>
+            {projects.map((project) => (
+                <div className="project" key={project.id}>
+                    <h4>{ project.title }</h4>
+                    <p>{ project.image }</p>
+                    <p>{ project.deployed }</p>
+                    <p>{ project.github }</p>
+                    <p>{ project.tech }</p>
+                </div>
+            ))}
+            </div>
         </div>
      );
 }
